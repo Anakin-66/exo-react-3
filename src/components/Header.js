8b961cfa29905953d2673d7df3b1f210d60db78a
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-09 16:11:55 +0100

    added link on contacts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,20 @@
 import ProfileMenu from "../ProfileMenu";
+import { Outlet, Link } from "react-router-dom";
 
 function Header() {
     return (
       <>
         <h1>Mon titre</h1>
         <ul className="puce" >
-            <li>Choix 1</li>
-            <li>Choix 2</li>
-            <li>Choix 3</li>
+            <li>
+              Choix 1
+            </li>
+            <li>
+              <Link to="/contact">Contacts</Link>
+            </li>
+            <li>
+              Choix 3
+            </li>
             <ProfileMenu />
         </ul>
       </>

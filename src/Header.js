34e3de2be4f344e4a01b0177787baa9cf786546f
Frangeul Nathan <seekerz17@outlook.fr>
2023-11-09 11:18:25 +0100

    added className and css

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import ProfileMenu from "./ProfileMenu";
+
+function Header() {
+    return (
+      <>
+        <h1>Mon titre</h1>
+        <ul className="puce" >
+            <li>Choix 1</li>
+            <li>Choix 2</li>
+            <li>Choix 3</li>
+            <ProfileMenu />
+        </ul>
+      </>
+    )
+  }
+
+  export default Header;
---
+++ src/ProfileMenu.js
@@ -0,0 +1,12 @@
+function ProfileMenu() {
+    const profile = {
+        firstname: "Nathan",
+        lastname: "Frangeul"
+    }
+
+    return (
+        <li> {profile.firstname + ` ` +  profile.lastname} </li>
+    )
+}
+
+export default ProfileMenu;
